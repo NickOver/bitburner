9c221ef0b807135ba6c614d292402e6caaf6761d
NickOver <piotr.zegot@gmail.com>
2023-06-02 23:13:05 +0200

    TMP

--- build/Main.js
+++ build/Main.js
@@ -1,4 +1,5 @@
 import { dump } from "Modules/Dumper";
 export async function main(ns) {
+    dump(ns, 'dupa');
     dump(ns, ns);
 }
--- src/Main.ts
+++ src/Main.ts
@@ -2,5 +2,6 @@ import { NS } from "Bitburner";
 import { dump } from "Modules/Dumper";
 
 export async function main(ns: NS) {
+  dump(ns, 'dupa');
   dump(ns, ns);
 }
